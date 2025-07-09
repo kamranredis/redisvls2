92f131b87fed6ddb37f2272e220f288f4a0f14d9
root <root@kamran-benchmark.europe-west2-a.c.central-beach-194106.internal>
2025-07-09 11:49:05 +0000

    added command for benchmark parallal

---
+++ benchmark_parallal.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Configuration parameters
+NUM_PROCESSES=25
+QUERY_COUNT=10000
+
+redis_hosts=("localhost" "localhost" "redis-12000.internal.cluster.kamran-default.demo.redislabs.com" "redis-13000.internal.cluster.kamran-default.demo.redislabs.com" "redis-14000.internal.cluster.kamran-default.demo.redislabs.com" "redis-15000.internal.cluster.kamran-default.demo.redislabs.com" "redis-16000.internal.cluster.kamran-default.demo.redislabs.com")
+redis_ports=(6379 6380 12000 13000 14000 15000 16000)
+
+for i in "${!redis_hosts[@]}"; do
+    echo "Starting ${NUM_PROCESSES} benchmark processes for ${redis_hosts[i]}:${redis_ports[i]} with ${QUERY_COUNT} queries each"
+    for j in $(seq 1 ${NUM_PROCESSES}); do
+        python benchmarkvl.py query --redis-host "${redis_hosts[i]}" --redis-port "${redis_ports[i]}" --max-workers 1 --algorithm hnsw --query-count ${QUERY_COUNT} &
+    done
+    wait
+    echo "Completed benchmarks for ${redis_hosts[i]}:${redis_ports[i]}"
+done
