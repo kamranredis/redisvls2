3982e80aa36051d70182225e3008d90edea9a36d
root <root@kamran-benchmark.europe-west2-a.c.central-beach-194106.internal>
2025-07-09 12:28:29 +0000

    modified parallal benchmark

--- benchmark_parallal.sh
+++ benchmark_parallal.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Configuration parameters
-NUM_PROCESSES=25
+NUM_PROCESSES=12
 QUERY_COUNT=10000
 
-redis_hosts=("localhost" "localhost" "redis-12000.internal.cluster.kamran-default.demo.redislabs.com" "redis-13000.internal.cluster.kamran-default.demo.redislabs.com" "redis-14000.internal.cluster.kamran-default.demo.redislabs.com" "redis-15000.internal.cluster.kamran-default.demo.redislabs.com" "redis-16000.internal.cluster.kamran-default.demo.redislabs.com")
-redis_ports=(6379 6380 12000 13000 14000 15000 16000)
+redis_hosts=( "localhost"  "redis-13000.internal.cluster.kamran-default.demo.redislabs.com" "redis-14000.internal.cluster.kamran-default.demo.redislabs.com" "redis-15000.internal.cluster.kamran-default.demo.redislabs.com" "redis-16000.internal.cluster.kamran-default.demo.redislabs.com")
+redis_ports=(6380  13000 14000 15000 16000)
 
 for i in "${!redis_hosts[@]}"; do
     echo "Starting ${NUM_PROCESSES} benchmark processes for ${redis_hosts[i]}:${redis_ports[i]} with ${QUERY_COUNT} queries each"
